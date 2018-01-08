4c8217fdcbd8b5370df0c95499bf42ce027e5db5
Jesse Wolfe! <jes5199@github.com>
2018-01-08 11:07:28 -0800

    extract rules to parameter instead of global

--- 1/1.hs
+++ 1/1.hs
@@ -17,11 +17,10 @@ iii_to_u str = shift_and_recur iii_to_u str
 drop_uu str@('U':'U':rest) = rest : (shift_and_recur drop_uu str)
 drop_uu str = shift_and_recur drop_uu str
 
-rules = [add_u, mx_to_mxx, iii_to_u, drop_uu]
 
 apply_rules_to_theorem rules str = nub $ concatMap (\f -> f str) rules
 
-all_theorums_from theorems = nub $ theorems ++ more_theorems ++ (all_theorums_from more_theorems)
+all_theorums_from rules theorems = nub $ theorems ++ more_theorems ++ (all_theorums_from rules more_theorems)
   where more_theorems = concatMap (apply_rules_to_theorem rules) theorems
 
 stop_at pred (x:rest) = if (pred x)
@@ -30,6 +29,7 @@ stop_at pred (x:rest) = if (pred x)
 
 main = do
   let start_with = [mi]
+  let rules = [add_u, mx_to_mxx, iii_to_u, drop_uu]
   let destination = is_mu
-  --let destination = (=="MIIU")
-  mapM_ putStrLn (stop_at destination $ all_theorums_from start_with)
+  -- let destination = (=="MIIU") -- to test the win condition
+  mapM_ putStrLn (stop_at destination $ all_theorums_from rules start_with)
